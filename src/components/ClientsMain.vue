b35498e2ea51d3470bd1081ca1db199060e0b67a
Bruno Braga <brunobragaw8t@gmail.com>
2022-09-21 08:50:43 +0100

    Add swiper to clients

--- src/components/ClientsMain.vue
+++ src/components/ClientsMain.vue
@@ -3,6 +3,10 @@ import { apiUrl } from "@/constants";
 import type { Client } from "@/interfaces";
 import { ref } from "vue";
 import ContentWrapper from "./ContentWrapper.vue";
+import { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/autoplay";
 
 const clients = ref([] as Array<Client>);
 
@@ -25,24 +29,32 @@ fetch(`${apiUrl}/client/?order=desc`)
 <template>
   <div v-if="clients && clients.length" class="py-8 bg-gray-100">
     <ContentWrapper>
-      <div class="flex gap-5 justify-center">
-        <div v-for="item in clients" :key="item.id" class="group">
-          <a v-if="item.url" :href="item.url" target="_blank">
+      <swiper
+        :modules="[Autoplay]"
+        :slides-per-view="2"
+        :breakpoints="{ 576: { slidesPerView: 3 }, 768: { slidesPerView: 4 } }"
+        :loop="true"
+        :autoplay="true"
+      >
+        <swiper-slide v-for="item in clients" :key="item.id">
+          <div class="flex justify-center">
+            <a v-if="item.url" :href="item.url" target="_blank" class="group">
+              <img
+                :src="item.image"
+                :alt="item.title"
+                class="w-28 h-20 object-contain grayscale transform-gpu transition-all group-hover:grayscale-0"
+              />
+            </a>
+
             <img
+              v-else
               :src="item.image"
               :alt="item.title"
               class="w-28 h-20 object-contain grayscale transform-gpu transition-all group-hover:grayscale-0"
             />
-          </a>
-
-          <img
-            v-else
-            :src="item.image"
-            :alt="item.title"
-            class="w-28 h-20 object-contain grayscale transform-gpu transition-all group-hover:grayscale-0"
-          />
-        </div>
-      </div>
+          </div>
+        </swiper-slide>
+      </swiper>
     </ContentWrapper>
   </div>
 </template>
